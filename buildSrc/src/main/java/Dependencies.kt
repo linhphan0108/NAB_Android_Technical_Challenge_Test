565d47ce63318bae4eaad115c14ea17bf223f77a
linhphan <linhphan0108@gmail.com>
2021-11-21 19:24:18 +0700

    - bump version 1.0.1 (2)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -10,8 +10,8 @@ object Configs {
     const val compileSdkVersion = 31
     const val minSdkVersion = 21
     const val targetSdkVersion = 31
-    const val versionCode = 1
-    const val versionName = "1.0.0"
+    const val versionCode = 2
+    const val versionName = "1.0.1"
 }
 
 object Versions{
